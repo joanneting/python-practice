548a7bd0af033964a4fb285715cd08e39e209a86
JoanneLin <j87101488@gmail.com>
2019-11-04 09:58:42 +0800

    guessNumber提示訊息修改

--- guessNumber.py
+++ guessNumber.py
@@ -4,7 +4,7 @@ min = 1
 max = 100
 while True :
     userGuess = int(input(f"請輸入一個介於 {min} ~ {max} 的值 :"))
-    if  max > userGuess > min : 
+    if  max >= userGuess >= min : 
         if userGuess > ans :
             max = userGuess 
         if userGuess < ans :
@@ -12,5 +12,5 @@ while True :
         if userGuess == ans :
             break
     else :
-        print(f"輸入數字範圍不在{min} ~ {max}")
+        print(f"輸入數字不在範圍內")
 print(f"恭喜猜中，答案為{ans}")
